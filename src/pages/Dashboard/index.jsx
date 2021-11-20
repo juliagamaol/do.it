8c44d7f3a2bfc068cc2fdff4bcf63dfc4362db92
Julia Gama de Oliveira <86054348+JuliaDevgama@users.noreply.github.com>
2021-11-20 01:07:04 -0300

    feat: creation of tasks and completion

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { Redirect } from 'react-router-dom';
 import Input from "../../components/Input"
@@ -7,18 +7,68 @@ import { Container, InputContainer, TasksContainer } from './styles';
 import {FiEdit2} from "react-icons/fi"
 import Card from '../../components/Card';
 import api from '../../services/api';
+import toast from 'react-hot-toast';
 
 export default function Dashboard({authenticated}) {
-    const [tasks, seTasks] = useState([])
-    const[token] = JSON.parse(localStorage.getItem("@Doit:token")) || ""
     const{register,handleSubmit} = useForm()
+    const [tasks, setTasks] = useState([])
+    const[token] = useState(JSON.parse(localStorage.getItem("@Doit:token")) || "")
 
     const loadTasks = () =>{
         api.get("/task",{
             headers:{
-                Authorization: `Bearer${token}`
+                Authorization: `Bearer ${token}`
+            },
+            params:{
+                completed:false
             }
         })
+            .then((res)=>{
+                const apiTasks = res.data.data.map((task)=>({
+                    ...task,
+                    createdAt:new Date(task.createdAt).toLocaleDateString("pt-BR",{
+                        day:"2-digit",
+                        month:"long",
+                        year:"numeric"
+                    })
+                }))
+               setTasks(apiTasks) 
+            })
+            .catch((err)=>console.log(err))
+    }
+
+    useEffect(()=>{
+        loadTasks()
+    },[])
+
+    const onSubmit = ({task}) =>{
+        if(!task){
+            return toast.error("Complete o campo para enviar uma tarefa")
+        }
+
+        api.post("/task",{
+            description:task
+        },
+        {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        }
+        )
+        .then((res)=>loadTasks())
+    }
+
+    const handleCompleted = id =>{
+        const newTasks = tasks.filter((task)=>task._id !== id)
+
+        api.put(`/task/${id}`,
+        {completed:true},
+        {
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+        })
+        .then((res)=>setTasks(newTasks))
     }
 
     if (!authenticated) {
@@ -26,7 +76,7 @@ export default function Dashboard({authenticated}) {
     }
     return (
         <Container>
-            <InputContainer>
+            <InputContainer onSubmit={handleSubmit(onSubmit)}>
                 <time>19 de novembro de 2021</time>
                 <section>
                     <Input 
@@ -39,7 +89,7 @@ export default function Dashboard({authenticated}) {
                 </section>
             </InputContainer>
             <TasksContainer>
-                {[1,2,3].map(_=> <Card title="Ola" date="19 de novembro de 2021" onClick={()=>{}}/>)}
+                {tasks.map(task=> <Card key={task._id} title={task.description} date={task.createdAt} onClick={()=>handleCompleted(task._id)}/>)}
             </TasksContainer>
         </Container>
     )
